1a47884568533b3ffb52ebe636246513b5d00810
reinerblock <reiner.block@gmail.com>
2018-09-13 18:50:39 +0200

    WS-2018.2.2 <Reiner@Majestix Create Tools.xml, IntelliLang.xml, ide.general.xml, git.xml, print.xml, textmate.xml, project.default.xml, Default copy.xml, other.xml, applicationLibraries.xml, railscasts_grey_comments.icls, Reiner.xml, file.template.settings.xml, xpath.xml, ui.lnf.xml, filetypes.xml, keymap.xml, Darcula _Reiner_.icls, code.style.schemes.xml, plugin_ui.xml, Deployment.xml, colors.scheme.xml, notifications.xml, github_settings.xml, TitaniumStudio.icls, JavaScript File.js, Editors.xml, abbreviations.xml, Default.xml, hg.xml, laf.xml, path.macros.xml, Solarized Dark.icls, Default _1_.xml, proxy.settings.xml, encoding.xml, editor.codeinsight.xml, Solarized Light.icls, editor.xml, Espresso _Reiner_.icls, webServers.xml, debugger.xml

---
+++ fileTemplates/internal/JavaScript File.js
@@ -0,0 +1,21 @@
+/**
+// <copyright file="${FILE_NAME}" company="Softelier Ltd.">
+// --------------------------------------------------------------------------------------------------------------------
+// Copyright (c) ${YEAR}-${YEAR} Softelier Ltd.. All Rights Reserved. Confidential.
+// --------------------------------------------------------------------------------------------------------------------
+// </copyright>
+//
+// Company      : Softelier Ltd.
+// Project      : ${PROJECT_NAME}
+// Module       : ${PROJECT_NAME}
+// File         : ${FILE_NAME}
+// Author       : ${USER} as independent developer for Softelier Ltd.
+// Creation date: ${DATE}
+// <summary>
+// Description  : 
+// </summary>
+// History
+// Date        Description
+// ${DATE}  Start implementation
+// --------------------------------------------------------------------------------------------------------------------
+*/
